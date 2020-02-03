9f0665c6cbbe6a2fbe5c696976fcc83590d732eb
Lucas Mbiwe <lucas@geek4good.com>
2020-02-03 22:13:18 +0000

    :wrench: configure the gatsby project

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,15 +1,46 @@
-require("dotenv").config({
-  path: `.env`,
-})
+require('dotenv').config();
+const {
+  api: { projectId, dataset }
+} = requireConfig('../studio/sanity.json');
+
 module.exports = {
   plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "@gatsbystorefront/gatsby-theme-storefront-shopify",
+      resolve: 'gatsby-source-sanity',
       options: {
-        shopName: process.env.GATSBY_SHOP_NAME,
-        accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
-        basePath: "/",
-      },
-    },
-  ],
+        projectId,
+        dataset,
+        // To enable preview of drafts, copy .env-example into .env,
+        // and add a token with read permissions
+        token: process.env.SANITY_READONLY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true
+      }
+    }
+  ]
+};
+
+/**
+ * We're requiring a file in the studio folder to make the monorepo
+ * work "out-of-the-box". Sometimes you would to run this web frontend
+ * in isolation (e.g. on codesandbox). This will give you an error message
+ * with directions to enter the info manually or in the environment.
+ */
+
+function requireConfig (path) {
+  try {
+    return require('../studio/sanity.json');
+  } catch (e) {
+    console.error(
+      'Failed to require sanity.json. Fill in projectId and dataset name manually in gatsby-config.js'
+    );
+    return {
+      api: {
+        projectId: process.env.SANITY_PROJECT_ID || '',
+        dataset: process.env.SANITY_DATASET || ''
+      }
+    };
+  }
 }
